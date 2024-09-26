7c1fc10feafd1139eb0741a890b5292af85eff1e
Rajat Mishra <77861069+mst-rajatmishra@users.noreply.github.com>
2024-09-26 15:10:49 +0530

    Update path sum.py

--- path sum.py
+++ path sum.py
@@ -9,22 +9,15 @@ class TreeNode:
 
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        def dfs(node,newTargetSum):
-            # Base case: if the tree is empty
-            if not node:
-                return False
-            newTargetSum = newTargetSum + node.val 
-            # If we reach a leaf node
-            if not node.left and not node.right:
-                return newTargetSum == targetSum
+        # Base case: if the tree is empty
+        if not root:
+            return False
+        
+        # If we reach a leaf node
+        if not root.left and not root.right:
+            return root.val == targetSum
         
         # Recur for left and right subtrees with the updated targetSum
-            
-            return (self.dfs(node.left, newTargetSum) or
-                self.dfs(node.right, newTargetSum))
-        return dfs(root,0)
-    
-t=TreeNode()
-s=Solution()
-ans=s.hasPathSum([1,2,3], 5)
-print(ans)
+        newTargetSum = targetSum - root.val
+        return (self.hasPathSum(root.left, newTargetSum) or
+                self.hasPathSum(root.right, newTargetSum))
